e97b0ec27b38559b9275e7cd9e35b0c690a73c47
Florian Klampfer <f.klampfer@gmail.com>
2013-12-04 12:26:59 +0100

    fx-step-1

---
+++ todo-javafx-client/src/main/java/io/ankor/tutorial/App.java
@@ -0,0 +1,5 @@
+package io.ankor.tutorial;
+
+public class App  {
+}
+
---
+++ todo-javafx-client/src/main/java/io/ankor/tutorial/TaskListController.java
@@ -0,0 +1,30 @@
+package io.ankor.tutorial;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TaskListController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // TODO
+    }
+
+    @FXML
+    public void newTodo(ActionEvent actionEvent) {
+        // TODO
+    }
+
+    @FXML
+    public void toggleAll(ActionEvent actionEvent) {
+        // TODO
+    }
+
+    @FXML
+    public void clearTasks(ActionEvent actionEvent) {
+        // TODO
+    }
+}
