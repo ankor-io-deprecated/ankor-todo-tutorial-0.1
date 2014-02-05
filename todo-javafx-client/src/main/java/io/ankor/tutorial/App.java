3e6425c990f0adb6fb8cc8876228c4ebdbf92228
Florian Klampfer <florian.klampfer@irian.at>
2014-02-05 16:32:25 +0100

    include step 1

--- todo-javafx-client/src/main/java/io/ankor/tutorial/App.java
+++ todo-javafx-client/src/main/java/io/ankor/tutorial/App.java
@@ -6,23 +6,31 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class App extends AnkorApplication {
+public class App  extends AnkorApplication {
 
+    // This is to start the JavaFX application.
     public static void main(String[] args) {
         launch(args);
     }
 
+    // This method gets called after a connection has been established
     @Override
     protected void startFXClient(Stage stage) throws Exception {
         stage.setTitle("Ankor Todo Sample");
+
+        // predefined fxml
         Pane myPane = FXMLLoader.load(getClass().getClassLoader().getResource("tasks.fxml"));
 
         Scene myScene = new Scene(myPane);
+
+        // predefined styles
         myScene.getStylesheets().add("style.css");
+
         stage.setScene(myScene);
         stage.show();
     }
 
+    // The WebSocket endpoint to connect to
     @Override
     protected String getWebSocketUri() {
         return "wss://ankor-todo-sample.irian.at/websocket/ankor";
