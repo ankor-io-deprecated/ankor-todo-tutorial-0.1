280a7398a91abbd3d3acd1fe721b7ba8941be367
Florian Klampfer <florian.klampfer@irian.at>
2014-02-05 16:38:17 +0100

    add step 3

--- todo-javafx-client/src/main/java/io/ankor/tutorial/TaskListController.java
+++ todo-javafx-client/src/main/java/io/ankor/tutorial/TaskListController.java
@@ -3,13 +3,15 @@ package io.ankor.tutorial;
 import at.irian.ankor.action.Action;
 import at.irian.ankor.annotation.ChangeListener;
 import at.irian.ankor.fx.binding.fxref.FxRef;
-import at.irian.ankor.fx.controller.FXControllerAnnotationSupport;
+import at.irian.ankor.fx.controller.FXControllerSupport;
 import at.irian.ankor.ref.Ref;
 import javafx.beans.property.Property;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.util.converter.NumberStringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -22,25 +24,34 @@ public class TaskListController implements Initializable {
     public Node footerTop;
     @FXML
     public Node footerBottom;
-    private FxRef modelRef;
+
+    @FXML
+    public Label todoCountNum;
+    @FXML
+    public Label todoCountText;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Ref rootRef = refFactory().ref("root");
+        FXControllerSupport.init(this, rootRef);
         rootRef.fire(new Action("init"));
-        FXControllerAnnotationSupport.scan(rootRef, this);
     }
 
     @ChangeListener(pattern = "root")
     public void myInit() {
         FxRef rootRef = refFactory().ref("root");
-        modelRef = rootRef.appendPath("model");
-
+        FxRef modelRef = rootRef.appendPath("model");
         FxRef footerVisibilityRef = modelRef.appendPath("footerVisibility");
 
         Property<Boolean> footerVisibilityProperty = footerVisibilityRef.fxProperty();
         footerTop.visibleProperty().bind(footerVisibilityProperty);
         footerBottom.visibleProperty().bind(footerVisibilityProperty);
+
+        todoCountText.textProperty().bind(modelRef.appendPath("itemsLeftText").<String>fxProperty());
+
+        todoCountNum.textProperty().bindBidirectional(
+                modelRef.appendPath("itemsLeft").<Number>fxProperty(),
+                new NumberStringConverter());
     }
 
     @FXML
