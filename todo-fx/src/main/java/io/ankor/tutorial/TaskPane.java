c4b8ebe43480b0ab2031deb60481cb1b39022da7
Florian Klampfer <florian.klampfer@irian.at>
2014-02-10 18:06:02 +0100

    task pane dummy

---
+++ todo-fx/src/main/java/io/ankor/tutorial/TaskPane.java
@@ -0,0 +1,10 @@
+package io.ankor.tutorial;
+
+import at.irian.ankor.fx.binding.fxref.FxRef;
+import javafx.scene.layout.AnchorPane;
+
+public class TaskPane extends AnchorPane {
+    public TaskPane(FxRef itemRef, int index) {
+
+    }
+}
