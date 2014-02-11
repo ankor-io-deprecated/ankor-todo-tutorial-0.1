8ea3e6375ae39a342a4030501493b8617e9740d6
Florian Klampfer <florian.klampfer@irian.at>
2014-02-11 18:36:49 +0100

    task pane dummy delete method

--- todo-fx/src/main/java/io/ankor/tutorial/TaskPane.java
+++ todo-fx/src/main/java/io/ankor/tutorial/TaskPane.java
@@ -1,10 +1,17 @@
 package io.ankor.tutorial;
 
 import at.irian.ankor.fx.binding.fxref.FxRef;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
 public class TaskPane extends AnchorPane {
     public TaskPane(FxRef itemRef, int index) {
+        // TODO
+    }
 
+    @FXML
+    public void delete(ActionEvent e) {
+        // TODO
     }
 }
