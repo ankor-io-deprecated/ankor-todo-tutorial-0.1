70f73ef7b02bb1595cc9fb24fd3ac479020aa532
Florian Klampfer <florian.klampfer@irian.at>
2014-02-17 16:55:00 +0100

    implement step 2

---
+++ todo-server/src/main/java/io/ankor/tutorial/viewmodel/TaskListModel.java
@@ -0,0 +1,9 @@
+package io.ankor.tutorial.viewmodel;
+
+import at.irian.ankor.ref.Ref;
+import io.ankor.tutorial.model.TaskRepository;
+
+public class TaskListModel {
+    public TaskListModel(Ref model, TaskRepository taskRepository) {
+    }
+}
