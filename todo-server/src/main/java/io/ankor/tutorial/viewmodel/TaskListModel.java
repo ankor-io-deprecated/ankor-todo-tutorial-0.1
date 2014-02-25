4af2a1545717484468c460c74aa6e41278e53b69
Florian Klampfer <florian.klampfer@irian.at>
2014-02-25 17:27:24 +0100

    implement step 5

--- todo-server/src/main/java/io/ankor/tutorial/viewmodel/TaskListModel.java
+++ todo-server/src/main/java/io/ankor/tutorial/viewmodel/TaskListModel.java
@@ -4,9 +4,13 @@ import at.irian.ankor.annotation.ActionListener;
 import at.irian.ankor.annotation.Param;
 import at.irian.ankor.messaging.AnkorIgnore;
 import at.irian.ankor.pattern.AnkorPatterns;
+import at.irian.ankor.ref.CollectionRef;
 import at.irian.ankor.ref.Ref;
 import io.ankor.tutorial.model.Task;
 import io.ankor.tutorial.model.TaskRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskListModel {
     @AnkorIgnore
@@ -19,6 +23,7 @@ public class TaskListModel {
     private Integer itemsLeft;
     private String itemsLeftText;
 
+    private List<TaskModel> tasks = new ArrayList<>();
 
     public TaskListModel(Ref modelRef, TaskRepository taskRepository) {
         AnkorPatterns.initViewModel(this, modelRef);
@@ -38,6 +43,24 @@ public class TaskListModel {
 
         int itemsLeft = taskRepository.getActiveTasks().size();
         modelRef.appendPath("itemsLeft").setValue(itemsLeft);
+
+        TaskModel model = new TaskModel(task);
+        tasksRef().add(model);
+    }
+
+    private CollectionRef tasksRef() {
+        return modelRef.appendPath("tasks").toCollectionRef();
+    }
+
+    @ActionListener
+    public void deleteTask(@Param("index") final int index) {
+        Task task = tasks.get(index);
+        taskRepository.deleteTask(task);
+
+        int itemsLeft = taskRepository.getActiveTasks().size();
+        modelRef.appendPath("itemsLeft").setValue(itemsLeft);
+
+        tasksRef().delete(index);
     }
 
     public Boolean getFooterVisibility() {
@@ -63,4 +86,13 @@ public class TaskListModel {
     public void setItemsLeftText(String itemsLeftText) {
         this.itemsLeftText = itemsLeftText;
     }
+
+    public List<TaskModel> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<TaskModel> tasks) {
+        this.tasks = tasks;
+    }
+
 }
