29ff4e3b50fa721429737b753160122ea616eb93
Florian Klampfer <f.klampfer@gmail.com>
2014-03-10 13:44:20 +0100

    provide task list model dummy

---
+++ todo-server/src/main/java/io/ankor/tutorial/viewmodel/TaskListModel.java
@@ -0,0 +1,10 @@
+package io.ankor.tutorial.viewmodel;
+
+import at.irian.ankor.ref.Ref;
+import io.ankor.tutorial.model.TaskRepository;
+
+public class TaskListModel {
+    public TaskListModel(Ref model, TaskRepository taskRepository) {
+        // TODO
+    }
+}
